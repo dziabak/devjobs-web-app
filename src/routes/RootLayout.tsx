03fda5301a3c3cc21eb8d2ce151ff76325b54d4a
dziabak <86873665+dziabak@users.noreply.github.com>
2023-11-16 14:43:48 +0100

    Fixed issue with missing ref for contract search input

--- src/routes/RootLayout.tsx
+++ src/routes/RootLayout.tsx
@@ -6,7 +6,7 @@ import { Outlet, Navigate } from "react-router-dom";
 const RootLayout = () => {
 	return (
 		<>
-			{/* <Navigate to="/jobs" /> */}
+			<Navigate to="/jobs" />
 			<Header />
 			<div className="relative px-8 pb-10 sm:px-0 bg-c-light-grey dark:bg-c-midnight">
 				<div className="container absolute h-0 px-8 -inset-y-10 -inset-x-1/2 sm:px-0">
